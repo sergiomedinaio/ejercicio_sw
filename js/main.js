1d1a906e3624769dd3cb72fa916bce6b3b58bbd4
Sergio Medina <hola@sergiomedina.io>
2023-04-24 22:24:03 -0300

    feat: add users table

--- js/main.js
+++ js/main.js
@@ -7,4 +7,31 @@ if (navigator.serviceWorker) {
 
 fetch('https://reqres.in/api/users')
     .then(response => response.json())
-    .then(json => console.log(json));
+    .then(({data}) => {
+        const content = document.querySelector('.content');
+        let table = `<table class="table table-striped table-dark table-border table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">First Name</th>
+                                <th scope="col">Last Name</th>
+                                <th scope="col">Avatar</th>
+                                <th scope="col">Email</th>
+                            </tr>
+                        </thead>
+                        <tbody>`;
+        data.forEach(user => {
+            table += `
+                <tr>
+                    <th scope="row">${user.id}</th>
+                    <td>${user.first_name}</td>
+                    <td>${user.last_name}</td>
+                    <td><img src="${user.avatar}" alt="${user.first_name} ${user.last_name}" class="img-thumbnail"></td>
+                    <td>${user.email}</td>
+                </tr>`;
+        });
+        table += `
+            </tbody>
+        </table>`;
+        content.innerHTML = table;
+    });
